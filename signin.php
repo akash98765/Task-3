0f2fe35efe33eab7ea8d08f2c54bfe74462876c6
akash98765 <sharmiladharsan@gmail.com>
2017-06-27 12:39:17 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,12 +1,12 @@
 <?php
- session_start();
+session_start();
 ?>
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="dev1.css">
-</head>
-<body>
-<?php 
+  <head>
+    <link rel="stylesheet" type="text/css" href="dev1.css">
+  </head>
+  <body>
+    <?php 
 $okay=0;
 $c=-1;
 $goto="welcome.php";
@@ -20,8 +20,8 @@ $dbc=@mysqli_connect($servername,$username,$password,$dbname);
 $sql="SELECT username,password FROM userlog";
 $result=mysqli_query($dbc,$sql);
 if($_SERVER["REQUEST_METHOD"]=="POST"){
- if(empty($_POST["uname"])||empty($_POST["pass"])){--$c;
- $err="Invalid credentials";
+if(empty($_POST["uname"])||empty($_POST["pass"])){--$c;
+$err="Invalid credentials";
 }
 else{$c=0;
 $uname=$_POST["uname"];
@@ -29,13 +29,11 @@ $pass=$_POST["pass"];
 while($row=mysqli_fetch_assoc($result)){
 if(($row["username"]==$uname) && ($row["password"]==$pass))
 {$okay=1;
- break;
+break;
 }
 }
 }
-
 }
-
 if($okay==0){
 $err="Invalid credentials";
 --$c;
@@ -43,24 +41,32 @@ $err="Invalid credentials";
 $_SESSION["uname"]=$uname;
 $_SESSION["pass"]=$pass;
 ?>
-<span class="s1"><?php echo $err; ?></span>
-<form method="post" action="<?php if($okay==0)echo htmlspecialchars($_SERVER['PHP_SELF']); else
- echo $goto; ?>">
-<fieldset>
-<legend>Login</legend>
-<p>
-<label for="a">
-UserName:</label><input type="text" name="uname" value="<?php echo $uname; ?>"><br>
-</p>
-<p>
-<label for="b">
-Password:</label><input type="password" name="pass" value="<?php echo $pass; ?>"><br>
-</p>
-<input type="submit" value="<?php if($c<0)
- echo 'okay'; else if($c==0)
- echo 'Click Again'; ?>">
-</fieldset>
-
-</form>
-</body>
+    <span class="s1">
+      <?php echo $err; ?>
+    </span>
+    <form method="post" action="<?php if($okay==0)echo htmlspecialchars($_SERVER['PHP_SELF']); else
+                                echo $goto; ?>">
+      <fieldset>
+        <legend>Login
+        </legend>
+        <p>
+          <label for="a">
+            UserName:
+          </label>
+          <input type="text" name="uname" value="<?php echo $uname; ?>">
+          <br>
+        </p>
+        <p>
+          <label for="b">
+            Password:
+          </label>
+          <input type="password" name="pass" value="<?php echo $pass; ?>">
+          <br>
+        </p>
+        <input type="submit" value="<?php if($c<0)
+                                    echo 'okay'; else if($c==0)
+                                    echo 'Click Again'; ?>">
+      </fieldset>
+    </form>
+  </body>
 </html>
