0add2e53d11659a4d12a60f92ef8afddeb014083
akash98765 <sharmiladharsan@gmail.com>
2017-06-27 12:37:29 +0530

    Update show.php

--- show.php
+++ show.php
@@ -2,31 +2,39 @@
 session_start();
 ?>
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="main.css">
-</head>
-<body>
-<ul>
-<li><a href="welcome.php">My Bin</a></li>
-<li><a href="task3.php">Sign Out-<?php echo $_SESSION["uname"] ?></a></li>
-</ul>
-<?php
- $id=$_GET["id"];
- $textvalue="";
- $servername="localhost";
- $username="root";
- $password="Weiss-2sfj";
- $dbname="users";
- $dbc=@mysqli_connect($servername,$username,$password,$dbname);
- $stmt="SELECT url,paste FROM userpaste";
- $result=mysqli_query($dbc,$stmt);
- while($row=mysqli_fetch_assoc($result)){if($id==$row["url"]){
- $textvalue=$row["paste"];
- break;
+  <head>
+    <link rel="stylesheet" type="text/css" href="main.css">
+  </head>
+  <body>
+    <ul>
+      <li>
+        <a href="welcome.php">My Bin
+        </a>
+      </li>
+      <li>
+        <a href="task3.php">Sign Out-
+          <?php echo $_SESSION["uname"] ?>
+        </a>
+      </li>
+    </ul>
+    <?php
+$id=$_GET["id"];
+$textvalue="";
+$servername="localhost";
+$username="root";
+$password="Weiss-2sfj";
+$dbname="users";
+$dbc=@mysqli_connect($servername,$username,$password,$dbname);
+$stmt="SELECT url,paste FROM userpaste";
+$result=mysqli_query($dbc,$stmt);
+while($row=mysqli_fetch_assoc($result)){if($id==$row["url"]){
+$textvalue=$row["paste"];
+break;
 }
 }
 ?>
-<textarea rows="25" cols="30" maxlength="3000"><?php echo $textvalue; ?></textarea>
-
-</body>
+    <textarea rows="25" cols="30" maxlength="3000">
+      <?php echo $textvalue; ?>
+    </textarea>
+  </body>
 </html>
