54fb16dbce8e9d47cee68364eea63bfb28bf8326
akash98765 <sharmiladharsan@gmail.com>
2017-06-29 19:11:34 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,51 +1,25 @@
 <?php
 session_start();
+$okay1=0;
+$okay=0;
+$_SESSION['secure']=rand(1000,9999);
 ?>
+<img src="hey.php">
 <html>
   <head>
     <link rel="stylesheet" type="text/css" href="dev1.css">
   </head>
   <body>
     <?php 
-$okay=0;
-$c=-1;
-$goto="welcome.php";
-$pass=$uname="";
-$err="";
-$username="root";
-$servername="localhost";
-$password="Weiss-2sfj";
-$dbname="users";
-$dbc=@mysqli_connect($servername,$username,$password,$dbname);
-$sql="SELECT username,password FROM userlog";
-$result=mysqli_query($dbc,$sql);
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-if(empty($_POST["uname"])||empty($_POST["pass"])){--$c;
+if($_SESSION['error']==0)
 $err="Invalid credentials";
-}
-else{$c=0;
-$uname=$_POST["uname"];
-$pass=$_POST["pass"];
-while($row=mysqli_fetch_assoc($result)){
-if(($row["username"]==$uname) && ($row["password"]==$pass))
-{$okay=1;
-break;
-}
-}
-}
-}
-if($okay==0){
-$err="Invalid credentials";
---$c;
-}
-$_SESSION["uname"]=$uname;
-$_SESSION["pass"]=$pass;
+else if($_SESSION['error1']==0)
+$err="Invalid captcha";
 ?>
     <span class="s1">
       <?php echo $err; ?>
     </span>
-    <form method="post" action="<?php if($okay==0)echo htmlspecialchars($_SERVER['PHP_SELF']); else
-                                echo $goto; ?>">
+    <form method="post" action="verify.php">
       <fieldset>
         <legend>Login
         </legend>
@@ -53,19 +27,24 @@ $_SESSION["pass"]=$pass;
           <label for="a">
             UserName:
           </label>
-          <input type="text" name="uname" value="<?php echo $uname; ?>">
+          <input type="text" name="uname"  required>
           <br>
         </p>
         <p>
           <label for="b">
             Password:
           </label>
-          <input type="password" name="pass" value="<?php echo $pass; ?>">
+          <input type="password" name="pass" required>
           <br>
         </p>
-        <input type="submit" value="<?php if($c<0)
-                                    echo 'okay'; else if($c==0)
-                                    echo 'Click Again'; ?>">
+        <p>
+          <label for="c">
+            Captcha:
+          </label>
+          <input type="text" name="cap" required>
+        </p>
+        <br>
+        <input type="submit" name="submit" value="submit">
       </fieldset>
     </form>
   </body>
