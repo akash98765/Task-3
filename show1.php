74631265e360862659ffabbcfa990df129a26448
akash98765 <sharmiladharsan@gmail.com>
2017-06-29 19:10:51 +0530

    Update show1.php

--- show1.php
+++ show1.php
@@ -1,8 +1,38 @@
 <html>
   <head>
-    <link rel="stylesheet" type="text/css" href="dev.css">
+    <link rel="stylesheet" type="text/css" href="styles/purebasic.css">
+    <style>
+      ul
+      {
+        list-style-type:none;
+        background-color:#333333;
+        padding:0px;
+        overflow:hidden;
+        margin:0px;
+      }
+      li
+      {
+        float:left;
+      }
+      li a
+      {
+        color:white;
+        text-decoration:none;
+        display:block;
+        padding:20px;
+      }
+      li a:hover
+      {
+        text-decoration:underline;
+        background-color:#111111;
+      }
+    </style>
   </head>
-  <body>
+  <body style="background-color:white;">
+    <script src="highlight.pack.js">
+    </script>
+    <script>hljs.initHighlightingOnLoad();
+    </script>
     <ul>
       <li>
         <a href="signup.php">Sign Up
@@ -21,16 +51,22 @@ $username="root";
 $password="Weiss-2sfj";
 $dbname="users";
 $dbc=@mysqli_connect($servername,$username,$password,$dbname);
-$stmt="SELECT url,paste FROM userpaste";
+$stmt="SELECT url,paste FROM userin";
 $result=mysqli_query($dbc,$stmt);
 while($row=mysqli_fetch_assoc($result)){if($id==$row["url"]){
 $textvalue=$row["paste"];
+$textvalue=htmlspecialchars($textvalue);
 break;
 }
 }
 ?>
-    <textarea rows="25" cols="30" maxlength="3000">
-      <?php echo $textvalue; ?>
-    </textarea>
+    <pre>
+<code>
+<?php echo $textvalue;
+?>
+<!--<?php echo $textvalue; ?>
+-->
+</code>
+</pre>
   </body>
 </html>
