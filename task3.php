eda0a4701322670d62e58865e35fedaf965a6c21
akash98765 <sharmiladharsan@gmail.com>
2017-06-29 19:12:58 +0530

    Update task3.php

--- task3.php
+++ task3.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+$_SESSION['error']=0;
+$_SESSION['error1']=1;
+$_SESSION['taken']=1;
 $go="task3.php";
 $servername="localhost";
 $username="root";
@@ -7,6 +10,8 @@ $password="Weiss-2sfj";
 $dbname="users";
 $dbc=@mysqli_connect($servername,$username,$password,$dbname);
 $id=0;
+$sql="DELETE FROM userin WHERE expirydate<NOW()";
+mysqli_query($dbc,$sql);
 ?>
 <html>
   <head>
@@ -30,23 +35,32 @@ $id=0;
     <div id="d1">
       <h2>Welcome!
       </h2>
+      <br>
+      <span>
+        <a href="signup.php">Sign Up
+        </a> to start using
+      </span>
       <!--<p id="p1"><a href="signup.php">Sign up</a> to start using</p>
 -->
     </div>
     <h4>Recent Posts:
       <br>
       <?php
-$sql="SELECT url FROM userpaste";
+$name="anonymous";
+$sql="SELECT url,user,type,visible FROM userin";
 $result=mysqli_query($dbc,$sql);
 while($row=mysqli_fetch_assoc($result)){
+if($row["visible"]!="Private"){
+if($row["type"]=="be known")
+{$name=$row["user"];
+} 
+else
+$name="anonymous";
 $id=$row["url"];
-echo "<br><a href='show1.php?id=$id'> $id </a><br>";
+echo "<br><a href='show1.php?id=$id'> $id-$name </a><br>";
+}
 }
 ?>
     </h4>
   </body>
-  <?php
-session_unset();
-session_destroy();
-?>
 </html>
