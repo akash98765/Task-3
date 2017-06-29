21c5edef38ac823973c6bb4a21bdac7cf81bacaa
akash98765 <sharmiladharsan@gmail.com>
2017-06-29 19:22:52 +0530

    Update show2.php

--- show2.php
+++ show2.php
@@ -2,65 +2,75 @@
 session_start();
 ?>
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="styles/purebasic.css">
-<style>
-ul
-{list-style-type:none;
- background-color:#333333;
- padding:0px;
- overflow:hidden;
- margin:0px;
+  <head>
+    <link rel="stylesheet" type="text/css" href="styles/purebasic.css">
+    <style>
+      ul
+      {
+        list-style-type:none;
+        background-color:#333333;
+        padding:0px;
+        overflow:hidden;
+        margin:0px;
+      }
+      li
+      {
+        float:left;
+      }
+      li a
+      {
+        color:white;
+        text-decoration:none;
+        display:block;
+        padding:20px;
+      }
+      li a:hover
+      {
+        text-decoration:underline;
+        background-color:#111111;
+      }
+    </style>
+  </head>
+  <body style="background-color:white;">
+    <script src="highlight.pack.js">
+    </script>
+    <script>hljs.initHighlightingOnLoad();
+    </script>
+    <ul>
+      <li>
+        <a href="welcome.php">My Bin
+        </a>
+      </li>
+      <li>
+        <a href="signout.php">Sign Out-
+          <?php echo $_SESSION['uname']; ?>
+        </a>
+      </li>
+    </ul>
+    <?php
+$id=$_GET["id"];
+$textvalue="";
+$servername="localhost";
+$username="root";
+$password="Weiss-2sfj";
+$dbname="users";
+$dbc=@mysqli_connect($servername,$username,$password,$dbname);
+$stmt="SELECT url,paste FROM userin";
+$result=mysqli_query($dbc,$stmt);
+while($row=mysqli_fetch_assoc($result)){if($id==$row["url"]){
+$textvalue=$row["paste"];
+$textvalue=htmlspecialchars($textvalue);
+break;
 }
-li
-{float:left;
 }
-li a
-{color:white;
- text-decoration:none;
- display:block;
- padding:20px;
-}
-li a:hover
-{text-decoration:underline;
- background-color:#111111;
-}
-
-</style>
-</head>
-<body style="background-color:white;">
-<script src="highlight.pack.js"></script>
-<script>hljs.initHighlightingOnLoad();</script>
-<ul>
-<li><a href="welcome.php">My Bin</a></li>
-<li><a href="signout.php">Sign Out-<?php echo $_SESSION['uname']; ?></a></li>
-</ul>
-<?php
- $id=$_GET["id"];
- $textvalue="";
- $servername="localhost";
- $username="root";
- $password="Weiss-2sfj";
- $dbname="users";
- $dbc=@mysqli_connect($servername,$username,$password,$dbname);
- $stmt="SELECT url,paste FROM userin";
- $result=mysqli_query($dbc,$stmt);
- while($row=mysqli_fetch_assoc($result)){if($id==$row["url"]){
- $textvalue=$row["paste"];
- $textvalue=htmlspecialchars($textvalue);
- break;
-}
-}
-
 ?>
-
-
-<pre>
+    <pre>
 <code>
 <?php echo $textvalue;
 ?>
 <!--<?php echo $textvalue; ?>
 -->
-</code></pre>
-</body>
+</code>
+</pre>
+  </body>
 </html>
