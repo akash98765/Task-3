1386fa881b9f42e68cd11d649415379b78adee42
akash98765 <sharmiladharsan@gmail.com>
2017-06-29 19:18:36 +0530

    Update aj.php

--- aj.php
+++ aj.php
@@ -1,20 +1,18 @@
 <?php 
- session_start();
- 
- $servername="localhost";
- $username="root";
- $password="Weiss-2sfj";
- $dbname="users";
- $dbc=@mysqli_connect($servername,$username,$password,$dbname);
- $stmt1="SELECT username FROM userlog";
- $verify=mysqli_query($dbc,$stmt1);
- $u=$_REQUEST['u'];
- while($row1=mysqli_fetch_assoc($verify)){
- if($u==$row1['username'])
- {echo "Username already taken";
-  $_SESSION['taken']=0;
- 
-  break;
+session_start();
+$servername="localhost";
+$username="root";
+$password="Weiss-2sfj";
+$dbname="users";
+$dbc=@mysqli_connect($servername,$username,$password,$dbname);
+$stmt1="SELECT username FROM userlog";
+$verify=mysqli_query($dbc,$stmt1);
+$u=$_REQUEST['u'];
+while($row1=mysqli_fetch_assoc($verify)){
+if($u==$row1['username'])
+{echo "Username already taken";
+$_SESSION['taken']=0;
+break;
 }
 else
 $_SESSION['taken']=1;
